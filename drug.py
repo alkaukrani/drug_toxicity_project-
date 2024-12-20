76356425b093d44f8836b600dc6fc0e843ae72f5
Alka Ukrani <adukrani@uwaterloo.ca>
2024-12-19 21:26:28 -0500

    intial commit

--- drug.py
+++ drug.py
@@ -1,11 +1,16 @@
-
 import pandas as pd
 import numpy as np
+
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.DataStructs.cDataStructs import ConvertToNumpyArray
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
 
+from rdkit.Chem import rdMolDescriptors # type: ignore
 from rdkit.Chem import MolFromSmiles # type: ignore
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
 
 
@@ -21,7 +26,7 @@ data = data.drop(columns=['NR-AR', 'NR-AR-LBD', 'NR-Aromatase', 'NR-ER-LBD', 'NR
 #removing missing values 
 data = data.dropna(subset=['NR-ER', 'SR-ARE', 'SR-MMP', 'smiles'])
 
-print(data.columns)
+
 
 
 # Generate Fingerprints
@@ -34,25 +39,27 @@ for i, s in enumerate(data['smiles']):
         print(f"Invalid SMILES at index {i}: {s}")
     else:
         
+        #valid smiles
         valid_indices.append(i)
         # Generate Morgan Fingerprints
-        fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=2048)
+        morgan_generator = rdMolDescriptors.GetMorganGenerator(radius=2, fpSize=2048)
+        # Initialize the Morgan generator for generating fingerprints
+
+
+     
+
+        fp = morgan_generator.GetFingerprint(mol)
         # Convert to numpy array
         fp_array = np.zeros((1, 2048))
-        ConvertToNumpyArray(fp, fp_array[0])
+        ConvertToNumpyArray(fp, fp_array[0]) #refers to the first row of the placeholder fp_array, where the fingerprint will be stored
         fingerprints.append(fp_array[0])
 
-print(dir(AllChem)) 
+
+
+
 
 # Keep only valid molecules
 data = data.iloc[valid_indices].reset_index(drop=True)
-       
-
-
-
-
-
-
 
 #storing converted SMILES
 x=np.array(fingerprints)
@@ -60,13 +67,20 @@ x=np.array(fingerprints)
 # Target Extraction (toxicity prediction)
 y = data[['NR-ER', 'SR-ARE', 'SR-MMP']].values
 
+gbm = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, random_state=42)
+# Train the model
 
+# Train the model
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+# Train the model
 
+gbm.fit(X_train, y_train)
 
+y_pred = gbm.predict(X_test)
 
-
-
-
-
-
-
+# Evaluate the model's accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f'\nAccuracy: {accuracy * 100:.2f}%')
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
